e5f7dc9b299f6f7ca68278b9bb1295fa2436844c
Reverie <patlavovach@gmail.com>
2019-12-16 17:19:50 +0200

    basic validation

--- wise-shopping-cart-container.js
+++ wise-shopping-cart-container.js
@@ -1,8 +1,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import '@polymer/iron-image/iron-image.js';
 import 'wise-shopping-cart/wise-shopping-cart.js';
-import '@polymer/paper-radio-button/paper-radio-button.js';
 import '@polymer/paper-radio-group/paper-radio-group.js';
+import '@polymer/paper-radio-button/paper-radio-button.js';
 import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-input/paper-input.js';
 
@@ -28,45 +27,37 @@ class WiseShoppingCartContainer extends PolymerElement {
         }
         h3 {
           padding: 0 .5em;
-
         }
         wise-shopping-cart {
           margin-right: .5em;
           flex: 1;
         }
-        .con-1 {
+        .shopping-cart-container {
           width: 80%;
-
         }
-        .con-2 {
+        .order-details-container {
           width: 20%;
         }
-
         .order-details {
           display: flex;
           flex-direction: column;
         }
-
         paper-card {
           flex: 1;
           margin-bottom: 1em;
           padding-bottom: .5em;
         }
-        
         span[slot=prefix] {
             margin-right: .5em;
         }
-        
         paper-input {
             padding: 0 1em;
         }
-        
         h1 {
           font-size: 1.8rem;
           font-weight: 600;
           text-transform: uppercase;
         }
-        
          paper-button {
             background-color: #fff;
             color: #000;
@@ -80,65 +71,64 @@ class WiseShoppingCartContainer extends PolymerElement {
             color: #fff;
             border: 2px solid #fff;
         }
-        
         .total-container {
             display: flex;
             flex-direction: column;
             align-items: flex-end;
         }
-
+        .order-details :nth-last-child(2) h3 {
+            margin-bottom: 0;
+        }
         @media (max-width: 750px) {
           .cart {
             flex-direction: column;
           }
-          .con-1, .con-2 {
+          .shopping-cart-container, .order-details-container {
             width: 100%;
           }
           wise-shopping-cart {
             margin-right: 0;
-            
           }
         }
       </style>
       <div>
         <div class="cart-container">
-        <div class="cart">
-          <div class="con-1">
-          <wise-shopping-cart></wise-shopping-cart>
-          </div>
-          <div class="con-2">
-          <div class="order-details">
-            <paper-card>
-              <h3>Тип доставки:</h3>
-              <paper-radio-group selected="Courier">
-                <paper-radio-button name="Courier">Кур'єр</paper-radio-button>
-                <paper-radio-button name="NewPost">Нова Пошта</paper-radio-button>
-                <paper-radio-button name="CashOnDelivery">Самовивіз</paper-radio-button>
-              </paper-radio-group>
-            </paper-card>
-            <paper-card>
-              <h3>Тип оплати:</h3>
-              <paper-radio-group selected="Online">
-                <paper-radio-button name="Online">Онлайн</paper-radio-button>
-                <paper-radio-button name="Cash">Готівкою</paper-radio-button>
-              </paper-radio-group>
-            </paper-card>
-            <paper-card>
-              <h3>Замовник:</h3>
-                <paper-input label="Ім'я"></paper-input>
-                <paper-input pattern="/^\\d{9}$/" label="Телефон">
-                    <span slot="prefix">+380</span>
-                </paper-input>
-                <paper-input label="Коментар"></paper-input>
-
-            </paper-card>
-            <div class="total-container">
-              <h1>СУМА: 750 грн</h1>
-              <paper-button>NEXT</paper-button>
+          <div class="cart">
+            <div class="shopping-cart-container">
+                <wise-shopping-cart cart-items="[[cartItems]]"></wise-shopping-cart>
+            </div>
+            <div class="order-details-container">
+              <div class="order-details">
+                <paper-card>
+                  <h3>Тип доставки:</h3>
+                  <paper-radio-group selected="Courier">
+                    <paper-radio-button name="Courier">Кур'єр</paper-radio-button>
+                    <paper-radio-button name="NewPost">Нова Пошта</paper-radio-button>
+                    <paper-radio-button name="CashOnDelivery">Самовивіз</paper-radio-button>
+                  </paper-radio-group>
+                </paper-card>
+                <paper-card>
+                  <h3>Тип оплати:</h3>
+                  <paper-radio-group selected="Online">
+                    <paper-radio-button name="Online">Онлайн</paper-radio-button>
+                    <paper-radio-button name="Cash">Готівкою</paper-radio-button>
+                  </paper-radio-group>
+                </paper-card>
+                <paper-card>
+                  <h3>Замовник:</h3>
+                    <paper-input label="Ім'я" required error-message="Заповніть, будь ласка, це поле"></paper-input>
+                    <paper-input pattern="^\\d{9}$" label="Телефон" required error-message="Заповніть, будь ласка, це поле">
+                        <span slot="prefix">+380</span>
+                    </paper-input>
+                    <paper-input label="Коментар"></paper-input>
+                </paper-card>
+                <div class="total-container">
+                  <h1>СУМА: 750 грн</h1>
+                  <paper-button on-tap="_proceed">NEXT</paper-button>
+                </div>
+              </div>
             </div>
           </div>
-          </div>
-        </div>
         </div>
       </div>
     `;
@@ -159,6 +149,18 @@ class WiseShoppingCartContainer extends PolymerElement {
 
   _isInShoppingCartAnyItems (cartItemsLength) {
     return cartItemsLength > 0;
+  }
+
+  _proceed () {
+    const inputs = this.shadowRoot.querySelectorAll('paper-input[required]');
+    let isValidCounter = 0;
+    inputs.forEach(input => {
+      if (input.validate()) {
+        isValidCounter ++;
+      }
+    });
+    const isValid = inputs.length === isValidCounter;
+    console.log(inputs, isValid);
   }
 
 }
